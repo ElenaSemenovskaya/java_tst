ca47ef05226136adbb740a9083ba2f2dba19088d
ElenaSemenovskaya <semenovskaya.elena@gmail.com>
2019-05-19 19:25:09 +0300

    1. Файл .java перемещен в пакет. 2. Внесены изменения по п.1 в конфигурационный файл

---
+++ sandbox/src/main/java/se/tst/sandbox/FirstProgram.java
@@ -0,0 +1,13 @@
+package se.tst.sandbox;
+
+public class FirstProgram {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, world");
+
+    System.out.println(2+2);
+    System.out.println(2+0);
+    System.out.println(2+"3");
+  }
+
+}
