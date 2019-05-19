ed8c85779b222aef5f869c6b6d781150694c9b28
ElenaSemenovskaya <semenovskaya.elena@gmail.com>
2019-05-19 19:29:33 +0300

    Файл.java вернулся в исходное состояние

--- sandbox/src/main/java/se/tst/sandbox/FirstProgram.java
+++ sandbox/src/main/java/se/tst/sandbox/FirstProgram.java
@@ -5,9 +5,6 @@ public class FirstProgram {
   public static void main(String[] args) {
     System.out.println("Hello, world");
 
-    System.out.println(2+2);
-    System.out.println(2+0);
-    System.out.println(2+"3");
-  }
+    }
 
 }
