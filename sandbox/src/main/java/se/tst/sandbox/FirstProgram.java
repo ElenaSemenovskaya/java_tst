6d4364986cdc43c7e3db1965afc2d9deeacbe7af
ElenaSemenovskaya <semenovskaya.elena@gmail.com>
2019-05-19 21:49:10 +0300

    Добавлены новые функции в FirstProgram.java

--- sandbox/src/main/java/se/tst/sandbox/FirstProgram.java
+++ sandbox/src/main/java/se/tst/sandbox/FirstProgram.java
@@ -3,8 +3,19 @@ package se.tst.sandbox;
 public class FirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world");
 
+    hello ("Elena");
+    hello ("User");
+
+    double c= 10;
+    double d = 20;
+    System.out.println("Площадь прямоугольника со сторонами " + c + " и " + d + "=" + area(c, d));
+    }
+  public static void hello(String Text){
+    System.out.println("Hello, " + Text + "!");
     }
 
+  public static double area (double a, double b) {
+    return a * b;
+  }
 }
