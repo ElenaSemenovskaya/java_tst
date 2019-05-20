02aecec6babd39ac61bba77ff75834a78b717ab3
ElenaSemenovskaya <semenovskaya.elena@gmail.com>
2019-05-20 23:16:56 +0300

    Вычисление расстояния между точками с использованием метода

--- sandbox/src/main/java/se/tst/sandbox/FirstProgram.java
+++ sandbox/src/main/java/se/tst/sandbox/FirstProgram.java
@@ -1,10 +1,14 @@
 package se.tst.sandbox;
 
+import com.sun.xml.internal.bind.v2.runtime.output.SAXOutput;
+import javafx.concurrent.Task;
+
 public class FirstProgram {
 
     public static void main(String[] args) {
       hello("Elena");
-      hello("User");
+      hello("Alexey");
+      task("2");
 
       Square s = new Square(50);
       System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
@@ -13,16 +17,17 @@ public class FirstProgram {
       System.out.println("Площадь прямоугольника со сторонами " + r.c + " и " + r.d + " = " + r.area());
 
       Point p1 = new Point (5, 5);
-      Point p2 = new Point (10,10);
-      System.out.println("Расстояние между точками = " + distance(p1, p2));
+      Point p2 = new Point (3,10);
+      System.out.println("Расстояние между точками с координатами " + "(" + p1.x + ";" + p1.y + ")" + ";" + "(" + p2.x + ";" + p2.y + ")" + " = " + p1.distance(p2));
     }
 
   public static void hello(String Text){
     System.out.println("Hello, " + Text + "!");
+
     }
-    
-  public static double distance (Point p1, Point p2) {
-    return Math.sqrt(Math.pow((p2.x-p1.x),2) + Math.pow((p2.y-p1.y),2));
-  }
+   public static void task (String Text) {
+     System.out.println("Решение задания № " + Text);
+   }
+
 
 }
--- sandbox/src/main/java/se/tst/sandbox/Point.java
+++ sandbox/src/main/java/se/tst/sandbox/Point.java
@@ -1,11 +1,15 @@
 package se.tst.sandbox;
 
 public class Point {
-  double x;
-  double y;
+  double x, y;
+
 
   public Point (double x, double y) {
     this.x = x;
     this.y = y;
+  }
+
+  public double distance (Point p2) {
+    return Math.sqrt(Math.pow((p2.x - this.x), 2) + Math.pow((p2.y - this.y), 2));
   }
 }
