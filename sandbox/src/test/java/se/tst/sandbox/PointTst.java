99d6574ff7ecce5f7c96d0936ba31db6a6dec131
ElenaSemenovskaya <semenovskaya.elena@gmail.com>
2019-05-22 00:34:23 +0300

    Проверка расстояния между точками через функцию Accert

--- sandbox/src/test/java/se/tst/sandbox/PointTst.java
+++ sandbox/src/test/java/se/tst/sandbox/PointTst.java
@@ -1,12 +1,14 @@
 package se.tst.sandbox;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 @Test
-public class PointTst {
-  public void testDistance () {
-    Point p1 = new Point(5,5);
-    Point p2 = new Point(3,10);
-    assert p1.distance(p2) == 5.39;
-  }
+  public class PointTst {
+    public void testDistance () {
+      Point p1 = new Point(5,5);
+      Point p2 = new Point(3,10);
+      Assert.assertEquals(p1.distance(p2),5.385164807134504);
+    }
+
 }
