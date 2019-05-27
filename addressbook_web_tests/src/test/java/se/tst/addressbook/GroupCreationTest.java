04e9bd4227b59f1dfa7ccdedf8431b1717fd0e71
ElenaSemenovskaya <semenovskaya.elena@gmail.com>
2019-05-27 23:43:28 +0300

    Поменяла драйвер Mozilla на Chrome

--- addressbook_web_tests/src/test/java/se/tst/addressbook/GroupCreationTest.java
+++ addressbook_web_tests/src/test/java/se/tst/addressbook/GroupCreationTest.java
@@ -1,6 +1,8 @@
 package se.tst.addressbook;
 
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,7 +13,7 @@ public class GroupCreationTest {
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
-    wd = new FirefoxDriver();
+    wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
