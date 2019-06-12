55d47b8089fda1ebd9ebd5df6bcb7e87182ccd8b
ElenaSemenovskaya <semenovskaya.elena@gmail.com>
2019-06-12 14:08:29 +0300

    4.2. Часть 1. Определение массива.

---
+++ sandbox/src/main/java/se/tst/addressbook/sandbox/Collections.java
@@ -0,0 +1,11 @@
+package se.tst.addressbook.sandbox;
+
+public class Collections {
+  public static void main (String[] args) {
+    String[] langs = {"Java", "Phyton", "C#", "PHP"};
+    for (String l : langs) {
+      System.out.println("Очень хочу выучить " + l);
+    }
+
+  }
+}
