f95580d382270fc83c55c91841102f9e89adac4d
ElenaSemenovskaya <semenovskaya.elena@gmail.com>
2019-06-12 14:32:19 +0300

    4.2. Часть 2. Пример использования класса для преобразования массива в списки

--- sandbox/src/main/java/se/tst/addressbook/sandbox/Collections.java
+++ sandbox/src/main/java/se/tst/addressbook/sandbox/Collections.java
@@ -1,9 +1,17 @@
 package se.tst.addressbook.sandbox;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Collections {
   public static void main (String[] args) {
     String[] langs = {"Java", "Phyton", "C#", "PHP"};
-    for (String l : langs) {
+
+    List<String> languages = Arrays.asList("Java", "Phyton", "C#", "PHP");
+      
+
+    for (String l : languages) {
       System.out.println("Очень хочу выучить " + l);
     }
 
