12ce3ff4230383edb0abebd2a79cde60be153f52
ElenaSemenovskaya <semenovskaya.elena@gmail.com>
2019-07-08 21:24:35 +0300

    В генератор контактов добавлена опция ком. строки

--- addressbook_web_tests/src/test/java/se/tst/addressbook/generators/ContactDataGenerator.java
+++ addressbook_web_tests/src/test/java/se/tst/addressbook/generators/ContactDataGenerator.java
@@ -1,5 +1,8 @@
 package se.tst.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import se.tst.addressbook.model.ContactDate;
 
 import java.io.File;
@@ -11,15 +14,31 @@ import java.util.List;
 
 public class ContactDataGenerator {
 
-    public static void main (String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
+    @Parameter(names = "-c", description = "GroupCount")
+    public int count;
 
-        List<ContactDate> contacts = generateContacts(count);
-        save(contacts, file);
+    @Parameter (names = "-f", description = "Target file")
+    public String file;
+
+    public static void main (String[] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+
     }
 
-    private static void save(List<ContactDate> contacts, File file) throws IOException {
+    private void run() throws IOException {
+        List<ContactDate> contacts = generateContacts(count);
+        save(contacts, new File(file));
+    }
+
+    private void save(List<ContactDate> contacts, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
         for (ContactDate contact : contacts){
@@ -30,7 +49,7 @@ public class ContactDataGenerator {
         writer.close();
     }
 
-    private static List<ContactDate> generateContacts(int count) {
+    private List<ContactDate> generateContacts(int count) {
         List<ContactDate> contacts = new ArrayList<ContactDate>();
         for (int i = 0; i < count; i++)
             contacts.add(new ContactDate().withName(String.format("Name %s", i)).withLastname(String.format("Lastame %s", i))
