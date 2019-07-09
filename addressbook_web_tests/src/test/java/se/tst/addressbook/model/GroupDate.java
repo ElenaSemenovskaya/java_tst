efe4b9d02e79399da910eaaf81e161cc252445c7
ElenaSemenovskaya <semenovskaya.elena@gmail.com>
2019-07-09 20:56:34 +0300

    6.6. Генерация данных о группе в xml. Загрузка данные из xml.

--- addressbook_web_tests/src/test/java/se/tst/addressbook/model/GroupDate.java
+++ addressbook_web_tests/src/test/java/se/tst/addressbook/model/GroupDate.java
@@ -1,8 +1,14 @@
 package se.tst.addressbook.model;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import java.util.Objects;
 
+@XStreamAlias("group")
+
 public class GroupDate {
+  @XStreamOmitField
   private int id = Integer.MAX_VALUE;
   private String name;
   private String header;
--- addressbook_web_tests/src/test/java/se/tst/addressbook/tests/GroupCreationTest.java
+++ addressbook_web_tests/src/test/java/se/tst/addressbook/tests/GroupCreationTest.java
@@ -1,6 +1,7 @@
 package se.tst.addressbook.tests;
 
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import se.tst.addressbook.model.GroupDate;
@@ -10,6 +11,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,15 +21,17 @@ public class GroupCreationTest extends TestBase {
 
   @DataProvider
   public Iterator<Object[]> validGroups () throws IOException {
-    List<Object[]> list= new ArrayList<Object[]>();
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+    String xml = "";
     String line = reader.readLine();
     while (line != null) {
-      String[] split = line.split(";");
-      list.add(new Object[]{(new GroupDate().withName(split[0]).withHeader(split[1]).withFooter(split[2]))});
+      xml += line;
       line = reader.readLine();
     }
-    return list.iterator();
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupDate.class);
+    List<GroupDate> groups = (List<GroupDate>) xstream.fromXML(xml);
+    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
 
