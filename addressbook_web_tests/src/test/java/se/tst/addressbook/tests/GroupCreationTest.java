8b9b3bf86dc79574effc4fc367f3c6c14844ddd4
ElenaSemenovskaya <semenovskaya.elena@gmail.com>
2019-07-13 20:13:20 +0300

    Создание группы. Извлечение данных для сравнения из БД.

--- addressbook_web_tests/src/test/java/se/tst/addressbook/tests/GroupCreationTest.java
+++ addressbook_web_tests/src/test/java/se/tst/addressbook/tests/GroupCreationTest.java
@@ -43,10 +43,10 @@ public class GroupCreationTest extends TestBase {
   @Test (dataProvider = "validGroups")
   public void testGroupCreation(GroupDate group) throws Exception {
     app.goTo().GroupPage();
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
   }
@@ -54,12 +54,11 @@ public class GroupCreationTest extends TestBase {
   @Test
   public void testBadGroupCreation() throws Exception {
     app.goTo().GroupPage();
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     GroupDate group = new GroupDate().withName("tst40' ");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size()));
-    Groups after = app.group().all();
-
+    Groups after = app.db().groups();
     assertThat(after, equalTo(before));
 
   }
