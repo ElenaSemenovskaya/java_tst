3fe014dc45d172ca4da4b3494951beb43ee2a0a2
ElenaSemenovskaya <semenovskaya.elena@gmail.com>
2019-07-15 14:24:30 +0300

    Предусловия в тест для добавления контакта в группу

--- addressbook_web_tests/src/test/java/se/tst/addressbook/tests/AddContactToGroup.java
+++ addressbook_web_tests/src/test/java/se/tst/addressbook/tests/AddContactToGroup.java
@@ -1,14 +1,43 @@
 package se.tst.addressbook.tests;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import se.tst.addressbook.model.ContactDate;
 import se.tst.addressbook.model.GroupDate;
+import se.tst.addressbook.model.Groups;
+
+import java.io.File;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasItem;
 
 public class AddContactToGroup  extends TestBase{
 
+    @BeforeTest
+    public void ensurePrecondition () {
+        Groups groups = app.db().groups();
+        app.goTo().scrollContact();
+        if (app.db().groups().size() == 0) {
+            app.goTo().GroupPage();
+            app.group().create(new GroupDate().withName("tst4"));
+        }
+        if (app.db().contacts().size() == 0) {
+            app.goTo().contactList();
+            app.contact().create(new ContactDate()
+                    .withName("Name1")
+                    .withLastname("LastName1")
+                    .withPhoto(new File("src/test/resources/fix.png"))
+                    .withAddress("Address")
+                    .withHomePhone("5555")
+                    .withMobilePhone("7777")
+                    .withWorkPhone("3333")
+                    .withMail("mail@mail")
+                    .withMail2("mail2")
+                    .withMail3("mail3")
+                    .inGroup(groups.iterator().next()), true);
+            app.goTo().scrollContact();
+        }
+    }
 
     @Test
     public void testAddContactToGroup () {
