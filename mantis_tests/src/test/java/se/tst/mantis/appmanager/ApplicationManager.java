46039d07c4a8d8b02ff720c077eec0c5536008d6
ElenaSemenovskaya <semenovskaya.elena@gmail.com>
2019-07-16 17:45:32 +0300

    8. Новый модуль для тестов для Mantis

--- mantis_tests/src/test/java/se/tst/mantis/appmanager/ApplicationManager.java
+++ mantis_tests/src/test/java/se/tst/mantis/appmanager/ApplicationManager.java
@@ -1,4 +1,4 @@
-package se.tst.addressbook.appmanager;
+package se.tst.mantis.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -19,12 +19,7 @@ public class ApplicationManager {
   private final Properties properties;
   public WebDriver wd;
 
-  private ContactHelper contactHelper;
-  private NavigationHelper navigationHelper;
-  private GroupHelper groupHelper;
-  private SessionHelper sessionHelper;
   private String browser;
-  private DbHelper dbHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -35,55 +30,18 @@ public class ApplicationManager {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
-    dbHelper = new DbHelper();
-
-    if (browser.equals(BrowserType.CHROME)){
+    if (browser.equals(BrowserType.CHROME)) {
       wd = new ChromeDriver();
     } else if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
-    } else  if (browser.equals(BrowserType.IE)) {
+    } else if (browser.equals(BrowserType.IE)) {
       wd = new InternetExplorerDriver();
     }
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
     wd.get(properties.getProperty("web.baseURL"));
-    groupHelper = new GroupHelper(wd);
-    navigationHelper = new NavigationHelper(wd);
-    sessionHelper = new SessionHelper(wd);
-    contactHelper = new ContactHelper(wd);
-    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
-}
-
-  public void logout() {
-    wd.findElement(By.linkText("Logout")).click();
   }
 
   public void stop() {
     wd.quit();
   }
-
-  public boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  public GroupHelper group() {
-    return groupHelper;
-  }
-
-  public ContactHelper contact() {
-    return contactHelper;
-  }
-
-  public NavigationHelper goTo() {
-    return navigationHelper;
-  }
-
-  public DbHelper db() {
-    return dbHelper;
-  }
-
 }
